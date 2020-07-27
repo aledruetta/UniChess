3f02b86f810f19b3e766012eda162d275735e7ec
Alejandro Druetta <aledruetta@gmail.com>
2020-07-27 18:17:39 -0300

    Editorconfig

--- Makefile
+++ Makefile
@@ -1,2 +1,8 @@
 install:
 	pip install -e .['dev']
+
+
+format:
+	isort **/*.py
+	black -l 79 **/*.py
+	flake8 **/*.py
---
+++ xadrez/app.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+from flask import Flask
+
+
+def create_app():
+    app = Flask(__name__)
+
+    return app
