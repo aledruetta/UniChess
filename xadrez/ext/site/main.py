42e3a1105eccf22422b9c8fc18e6ac6bd40870bf
Alejandro Druetta <aledruetta@gmail.com>
2020-08-04 13:17:25 -0300

    Formating chess board

--- xadrez/ext/site/main.py
+++ xadrez/ext/site/main.py
@@ -4,12 +4,37 @@ from flask import Blueprint, render_template
 bp = Blueprint('site', __name__)
 
 
+def start_position():
+    cell = [[' ' for _ in range(8)] for _ in range(8)]
+    cell[0] = ['♜', '♞', '♝', '♛', '♚', '♝', '♞', '♜']
+    cell[1] = ['♟' for _ in range(8)]
+    cell[-1] = ['♖', '♘', '♗', '♕', '♔', '♗', '♘', '♖']
+    cell[-2] = ['♙' for _ in range(8)]
+
+    return cell
+
+
+def colors():
+    lst = []
+    inc = 0
+    for i in range(8):
+        lst.append([])
+        for j in range(8):
+            if (i * 8 + j + inc) % 2 == 0:
+                color = 'has-background-black has-text-white'
+            else:
+                color = 'has-background-white has-text-black'
+            lst[i].append(color)
+        inc += 1
+
+    return lst
+
+
 @bp.route('/')
 def index():
-    cell = [['.' for _ in range(8)] for _ in range(8)]
-
     return render_template(
         'index.html',
         title='UniChess',
-        cell=cell,
+        cell=start_position(),
+        color=colors(),
     )
