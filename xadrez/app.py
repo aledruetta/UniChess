d45a1771db9a4c9e6fad66e97889b8a27dd928cb
Alejandro Druetta <aledruetta@gmail.com>
2020-08-13 10:22:25 -0300

    Debug toolbar extension

--- xadrez/app.py
+++ xadrez/app.py
@@ -2,11 +2,13 @@
 
 from flask import Flask
 
-from xadrez.ext import site
+from xadrez.ext import site, toolbar
 
 
 def create_app():
     app = Flask(__name__)
+    app.config["SECRET_KEY"] = "abacate01"
+    toolbar.init_app(app)
     site.init_app(app)
 
     return app
