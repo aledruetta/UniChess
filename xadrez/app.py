8f17ca562b6fdef18195bc852b44f75a27a23e81
Alejandro Druetta <aledruetta@gmail.com>
2020-08-13 10:26:45 -0300

    Config extension

--- xadrez/app.py
+++ xadrez/app.py
@@ -2,12 +2,12 @@
 
 from flask import Flask
 
-from xadrez.ext import site, toolbar
+from xadrez.ext import site, toolbar, config
 
 
 def create_app():
     app = Flask(__name__)
-    app.config["SECRET_KEY"] = "abacate01"
+    config.init_app(app)
     toolbar.init_app(app)
     site.init_app(app)
 
---
+++ xadrez/ext/config/__init__.py
@@ -0,0 +1,2 @@
+def init_app(app):
+    app.config["SECRET_KEY"] = "abacate01"
