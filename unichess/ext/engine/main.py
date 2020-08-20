bc8594c04e191798f4aca2a190ab7e0e319970dd
Alejandro Druetta <aledruetta@gmail.com>
2020-08-20 17:22:50 -0300

    Refactor engine

--- unichess/ext/engine/main.py
+++ unichess/ext/engine/main.py
@@ -1,44 +1,63 @@
+from random import randint
+
 import chess
 import chess.svg
 
 from unichess.ext.db import db
 from unichess.ext.db.models import Board, Movement
 
+MIN_ID = 1
+MAX_ID = 2 ** 63
+
 
 class UniBoard(chess.Board):
     def __init__(self, random_id=None, fen=chess.STARTING_FEN):
         super().__init__(fen)
-        self.random_id = random_id
+
         self.db_board_id = None
+        self.random_id = random_id
         self.turn = chess.WHITE
 
-    def db_create_board(self):
+        if not random_id:
+            self.create_board()
+        else:
+            self.load_board()
+
+    def create_board(self):
+        self.random_id = randint(MIN_ID, MAX_ID)
+
         db_board = Board(random_id=self.random_id)
         db.session.add(db_board)
         db.session.commit()
 
         self.db_board_id = db_board.id
 
-    def db_load_board(self, random_id):
-        db_board = Board.query.filter_by(random_id=random_id).first()
+    def load_board(self):
+        db_board = Board.query.filter_by(random_id=self.random_id).first()
         self.db_board_id = db_board.id
         db_movements = Movement.query.filter_by(
             board_id=self.db_board_id
         ).all()
         for movement in db_movements:
-            self.uni_move(movement.uci)
+            movement = chess.Move.from_uci(movement.uci)
+            self.push(movement)
 
-    def db_save_movement(self, uci):
+    def save_movement(self, uci):
         movement = Movement(
             uci=uci, color=self.turn, board_id=self.db_board_id
         )
         db.session.add(movement)
         db.session.commit()
 
-    def uni_render(self):
-        return chess.svg.board(board=self)
-
-    def uni_move(self, uci):
+    def move(self, uci):
         movement = chess.Move.from_uci(uci)
         if movement in self.legal_moves:
             self.push(movement)
+            self.save_movement(uci)
+
+    @staticmethod
+    def render_base():
+        return chess.svg.board(chess.BaseBoard())
+
+    def render(self):
+        return chess.svg.board(board=self)
--- unichess/ext/site/main.py
+++ unichess/ext/site/main.py
@@ -1,14 +1,9 @@
-from random import randint
-
 from flask import Blueprint, redirect, render_template, request, url_for
 from wtforms import Form, StringField, validators
 
 from unichess.ext.engine import UniBoard
 
 bp = Blueprint("site", __name__)
-
-MIN_ID = 1
-MAX_ID = 2 ** 63
 
 
 class MoveForm(Form):
@@ -24,31 +19,29 @@ def index():
     form = PlayForm(request.form)
 
     if request.method == "POST":
-        random_id = randint(MIN_ID, MAX_ID)
-        uniboard = UniBoard(random_id)
-        uniboard.db_create_board()
-
-        return redirect(url_for("site.board", random_id=random_id))
+        uniboard = UniBoard()
+        return redirect(url_for("site.board", random_id=uniboard.random_id))
 
     return render_template(
-        "index.html", title="UniChess", board="", form=form,
+        "index.html",
+        title="UniChess",
+        board=UniBoard.render_base(),
+        form=form,
     )
 
 
 @bp.route("/board/<int:random_id>", methods=["GET", "POST"])
 def board(random_id):
     form = MoveForm(request.form)
-    uniboard = UniBoard()
-    uniboard.db_load_board(random_id)
+    uniboard = UniBoard(random_id)
 
     if request.method == "POST" and form.validate():
         uci = form.movement.data
-        uniboard.uni_move(uci)
-        uniboard.db_save_movement(uci)
+        uniboard.move(uci)
 
     return render_template(
         "board.html",
         title="UniChess Board",
-        board=uniboard.uni_render(),
+        board=uniboard.render(),
         form=form,
     )
