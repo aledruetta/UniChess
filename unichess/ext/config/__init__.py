c1c9916fab2fa2a1cd054eac9e8f08f0389d3a76
Alejandro Druetta <aledruetta@gmail.com>
2020-08-20 16:12:09 -0300

    Sqlalchemy track modifications false

--- unichess/ext/config/__init__.py
+++ unichess/ext/config/__init__.py
@@ -2,6 +2,7 @@ def init_app(app):
     app.config["SECRET_KEY"] = "abacate01"
     app.config["DEBUG_TB_ENABLED"] = True
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     if app.debug:
         app.config["DEBUG_TB_TEMPLATE_EDITOR_ENABLED"] = True
