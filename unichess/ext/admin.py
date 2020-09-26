5ad2db4c2ad00fcb3aa77477265996cc113a6d68
Alejandro Druetta <aledruetta@gmail.com>
2020-09-26 20:06:55 -0300

    Admin View is_accessible method

--- unichess/ext/admin.py
+++ unichess/ext/admin.py
@@ -1,14 +1,33 @@
+from flask import redirect, request, url_for
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
+from flask_login import current_user, login_required
 
 from .auth import User
 from .db import db
 from .engine import Board, Movement
 
-admin = Admin(name="UniChess Admin", template_mode="bootstrap3")
-admin.add_view(ModelView(User, db.session))
-admin.add_view(ModelView(Board, db.session))
-admin.add_view(ModelView(Movement, db.session))
+
+class AdminView(ModelView):
+
+    @login_required
+    def is_accessible(self):
+        if current_user.is_authenticated and current_user.is_admin:
+            return True
+        return False
+
+    def inaccessible_callback(self, name, **kwargs):
+        if not self.is_accessible():
+            return redirect(url_for("auth.login", next=request.url))
+
+
+admin = Admin(
+    name="Dashboard",
+    template_mode="bootstrap3",
+)
+admin.add_view(AdminView(User, db.session))
+admin.add_view(AdminView(Board, db.session))
+admin.add_view(AdminView(Movement, db.session))
 
 
 def init_app(app):
